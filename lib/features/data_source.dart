fb13c8361e0785c117518a33d5676f0aa414860b
Haneen <haneensamak565@gmail.com>
2025-09-07 15:56:56 +0300

    Haneen Mohsen message

--- lib/features/data_source.dart
+++ lib/features/data_source.dart
@@ -16,6 +16,21 @@ final List<Person> persons = [
         "In today’s fast-paced digital world, technology continues to shape the way we live, work, and connect with each other. From the moment we wake up and check our phones, to the late hours when we browse the internet before going to sleep, technology surrounds us and influences almost every aspect of our daily lives. It provides us with tools that make communication faster, access to information easier, and entertainment more interactive than ever before. However, this convenience also comes with challenges, such as the risk of distraction, the spread of misinformation, and the growing concerns around privacy and data security.",
     screenColor: ColorPallete.deepOrange,
   ),
-  Person(name: "mahmoud", message: "I am here", image: "download".jpg , screenColor: ColorPallete.cyan ),
-  Person(name: "Mahmoud saeed", message: "dfasdf sdfasd ", image: "download".jpg, screenColor: ColorPallete.lightBlue),
+  Person(
+    name: "mahmoud",
+    message: "I am here",
+    image: "download".jpg,
+    screenColor: ColorPallete.cyan,
+  ),
+  Person(
+    name: "Mahmoud saeed",
+    message: "dfasdf sdfasd ",
+    image: "download".jpg,
+    screenColor: ColorPallete.lightBlue,
+  ),
+  Person(
+    name: "Haneen mohsen",
+    message:
+        "I would like to sincerely thank you for the great effort you put in with us throughout the course. You were always keen to explain in a simple and easy way, and that really helped me benefit and understand the framework more deeply. Honestly, you were the reason I started to love the field even more. Thank you so much for your patience, your support, and for answering all my questions. I wish you all the best in your career",
+  ),
 ];
