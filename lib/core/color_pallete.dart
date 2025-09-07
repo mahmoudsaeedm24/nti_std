b48e5b9d321d65352881c9432f6dfa7d0275f5cb
TOoka-M <tqyshkr@gmail.com>
2025-09-07 16:53:13 +0300

    Added my screen(Toka)

--- lib/core/color_pallete.dart
+++ lib/core/color_pallete.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-enum ColorPallete { pink, lightGreen, cyan, lightBlue, deepOrange, def, }
+enum ColorPallete { pink, purple, lightGreen, cyan, lightBlue, deepOrange, def }
 
 class AppColors {
   AppColors._();
   static const Color pink = Colors.pink;
+  static const Color purple = Color.fromARGB(255, 230, 210, 255);
   static const Color lightGreen = Colors.lightGreen;
   static const Color cyan = Colors.cyan;
   static const Color lightBlue = Colors.lightBlue;
@@ -14,13 +15,14 @@ class AppColors {
   static Color getColor({required ColorPallete colorPallte}) {
     return switch (colorPallte.name) {
       "pink" => AppColors.pink,
+      "purple" => AppColors.purple,
       "lightGreen" => AppColors.lightGreen,
       "cyan" => AppColors.cyan,
       "ligthBlue" => AppColors.lightBlue,
       "deepOrange" => AppColors.deepOrange,
       "def" => AppColors.def,
 
-      _ => const Color.fromARGB(255, 143, 143, 143)
+      _ => const Color.fromARGB(255, 143, 143, 143),
     };
   }
 }
