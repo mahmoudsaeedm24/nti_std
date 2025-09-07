d587bf17c628ed17ccc4a55cd3fd888d662d194e
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-09-07 23:05:03 +0300

    ahmed abomousa's message commit comming ..........

--- lib/features/data_source.dart
+++ lib/features/data_source.dart
@@ -15,6 +15,17 @@ May Allah reward him abundantly, bless his knowledge, and place all his efforts 
     screenColor: ColorPallete.cyan,
   ),
   Person(
+    name: "Ahmed Abomousa",
+    message: """ ✨ A Message of Gratitude and Appreciation ✨
+
+When I first joined NTI, I didn’t expect to gain much in terms of knowledge and learning. But Alhamdulillah, Allah blessed me with meeting you, and I benefited from you greatly — scientifically, morally, and ethically. May Allah bless you and grant you success, Insha’Allah.
+
+And if the only thing I took away from NTI was meeting a beautiful and delightful soul like yours, that alone would be more than enough for me. Thank you so much, and may Allah grant you success in all that lies ahead.
+""",
+    // image: null,
+    screenColor: ColorPallete.cyan,
+  ),
+  Person(
     name: "Rawan Magdy",
     message: """Good morning Eng Mohamed Emad ,
 I just wanted to take a moment to sincerely thank you, Your respectful attitude, the way you explain things, and how you treat us as students have really left a positive impact on me. Your teaching style is clear and inspiring, and I have truly benefited and learned a lot from you. The religious stories you shared reminded me of one of my old teachers who had the same respectful and kind approach. Your words and manner have touched me and left a good mark in my life. I really hope that one day I will have the chance to work with you in the future and I wish you good in your life and happiness always and success""",
