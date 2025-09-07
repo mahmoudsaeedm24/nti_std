f45884f1f0829872b96b2d952a67b805212cb23a
mahmoudm24 <mahmoudsaeedm24@gmail.com>
2025-09-07 16:37:20 +0300

    add mahmoud abu-zaid message

--- lib/features/data_source.dart
+++ lib/features/data_source.dart
@@ -8,7 +8,8 @@ final List<Person> persons = [
     message: """ ✨ A Message of Gratitude and Appreciation ✨
 I would like to sincerely thank Eng. Mohamed Emad for his tremendous effort in teaching us and for never falling short in any aspect. He truly gave his best and even more 🌹.
 I deeply appreciate his high morals, respectful attitude towards each one of us, and his continuous encouragement to always give our maximum effort. Most importantly, his constant reminders to keep God in our hearts ❤️.
-May Allah reward him abundantly, bless his knowledge, and place all his efforts in the scale of his good
+May Allah reward him abundantly, bless his knowledge, and place all his efforts in the scale of his good,
+بارك الله فيك
 """,
     // image: null,
     screenColor: ColorPallete.cyan,
@@ -17,5 +18,10 @@ May Allah reward him abundantly, bless his knowledge, and place all his efforts 
     name: "Rawan Magdy",
     message: """Good morning Eng Mohamed Emad ,
 I just wanted to take a moment to sincerely thank you, Your respectful attitude, the way you explain things, and how you treat us as students have really left a positive impact on me. Your teaching style is clear and inspiring, and I have truly benefited and learned a lot from you. The religious stories you shared reminded me of one of my old teachers who had the same respectful and kind approach. Your words and manner have touched me and left a good mark in my life. I really hope that one day I will have the chance to work with you in the future and I wish you good in your life and happiness always and success""",
+  ),
+  Person(
+    name: "Mahmoud Abu-zaid",
+    message:
+        "شكرا يا بشمهندس حضرتك كنت خير معلم اتعلمت من حضرتك حجات كتير مش بس فمحتوى الكورس بل واتعلمت من أخلاقك. حضرت شخصية خلوقه  انا سعيد جدا  بجد انى كنت مع حضرتك  ربنا يبارك فيك واسمع عن حضرتك كل خير  .",
   ),
 ];
